1f00acf9c23f6cf4e5e697346ef1419f632eaced
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-04-21 14:57:03 -0400

    Done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
+
+
 const init = () => {
-  
+ 
+  const form = document.querySelector("form");
+  form.addEventListener("submit",(event) =>{ 
+    event.preventDefault() 
+    const input = document.querySelector("input#searchByID")
+   fetch( `http://localhost:3000/movies/${input.value}`).then(res => res.json()).then(data => {
+    const title = document.querySelector("section#movieDetails h4") 
+    const summary = document.querySelector("section#movieDetails p")
+
+    title.innerText = data.title;
+    summary.innerText = data.summary;
+  })
+});
 }
 
 document.addEventListener('DOMContentLoaded', init);
